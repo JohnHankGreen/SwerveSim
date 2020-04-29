42276ea4d05df9d487d9b5037df8e9593a4bc2cc
Kaia <kaianook73@gmail.com>
2020-04-29 14:24:26 -0400

    Read 0, fixed screen size to my liking

--- static/constants.js
+++ static/constants.js
@@ -1,6 +1,6 @@
 // width and height of the robot driving around area in pixels
-const SCREEN_WIDTH = 600; // decrease this to shrink the width of the drivable area
-const SCREEN_HEIGHT = 600; // decrease this to shrink the height of the drivable area
+const SCREEN_WIDTH = 725; // decrease this to shrink the width of the drivable area
+const SCREEN_HEIGHT = 725; // decrease this to shrink the height of the drivable area
 
 // pixels per meter, for drawing the robot itself
 const POSITION_SCALE_FACTOR = 75.0; // decrease this to make the robot appear smaller
