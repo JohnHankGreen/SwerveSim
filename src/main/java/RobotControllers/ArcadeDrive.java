0a8aa5e7d175310cb5cc9650eb329616dd5abbb9
Kaia <kaianook73@gmail.com>
2020-04-30 10:25:02 -0400

    Completed 1st task, but drives weird

---
+++ src/main/java/RobotControllers/ArcadeDrive.java
@@ -0,0 +1,39 @@
+package RobotControllers;
+
+import interfaces.JoysticksInterface;
+import interfaces.RobotController;
+import interfaces.RobotInterface;
+import interfaces.SwerveWheelInterface;
+
+import java.util.List;
+
+public class ArcadeDrive implements RobotController {
+    @Override // this tells Java that the `loop` method implements the `loop` method specified in `RobotController`
+    public void loop(JoysticksInterface joysticks, RobotInterface robot) {
+        double leftVelocity = joysticks.getLeftStick().y * 5.0; // get the velocity of the robot going forward/backward
+        double rightVelocity = joysticks.getRightStick().x * 5.0; // get the velocity of the wheels that turn the robot
+
+        // this is a list of the swerve modules that are on the robot
+        List<SwerveWheelInterface> drivetrain = robot.getDrivetrain();
+
+
+        for(int i = 0; i < drivetrain.size(); i++) { // loop through each swerve module
+            SwerveWheelInterface wheel = drivetrain.get(i); // the swerve module we are looking at
+
+            wheel.setWheelAngle(0); // make sure the wheels are always facing forward (none of this funny swerve business)
+
+            // use the x position of the wheel (with the center of the robot being (0, 0)) to see if the wheel is on the right half of the robot or the left half
+            if(wheel.getPosition().x > 0) { // positive x is to the right, negative x is to the left
+                wheel.setWheelVelocity(leftVelocity); // Drive forward
+            } else {
+                wheel.setWheelVelocity(leftVelocity); // Drive backward
+            }
+
+            if(joysticks.getRightStick().x > 0 && wheel.getPosition().x < 0) {
+                wheel.setWheelVelocity(rightVelocity); // Turns right supposedly
+            } else if(joysticks.getRightStick().x < 0 && wheel.getPosition().x > 0){
+                wheel.setWheelVelocity(-(rightVelocity)); // Turns left supposedly
+            }
+        }
+    }
+}
